b980258c7a0b2f05c1bc651ea09266a2775587d0
467892746 <467892746@qq.com>
2019-03-25 23:37:12 +0800

    创建视图login

--- views.py
+++ views.py
@@ -1,6 +1,10 @@
 from django.http import HttpResponse
-
+from django.shortcuts import redirec
 
 
 def inde(request):
     return HttpResponse('index')
+
+
+def login(request):
+    return redirect('/index')
